4b65377a47045e2be113505001511f4f688ceacc
Altus Szawlowski <altusszawlowski@gmail.com>
2021-04-30 17:11:07 -0400

    inserted googleservices

--- components/Map.js
+++ components/Map.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
 
 export default function Map() {
   return (
     <View style={styles.container}>
       <MapView
-          provider={PROVIDER_GOOGLE}
-          style={StyleSheet.absoluteFillObject}
-          initialRegion={{
-            latitude: 40.6734,
-            longitude: -74.0083,
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05,
-          }}
-          loadingEnabled
-          loadingBackgroundColor="white"
-          loadingIndicatorColor="black"
-        >
-        </MapView>
+        provider={PROVIDER_GOOGLE}
+        style={StyleSheet.absoluteFillObject}
+        initialRegion={{
+          latitude: 40.6734,
+          longitude: -74.0083,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+        loadingEnabled
+        loadingBackgroundColor="white"
+        loadingIndicatorColor="black"
+      ></MapView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  Map: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   Map: {
+//     // flex: 1,
+//     // backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+// });
