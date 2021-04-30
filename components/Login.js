5b455a7da30f457d9ac2f6276304185c0ed7e210
Kevin Kim <kevin.kim9411@gmail.com>
2021-04-30 11:54:50 -0400

    fixed the error: Invariant Violation: Tried to register two views with the same name RNCSafeAreaProvider

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
-} from "@react-native-community/google-signin";
+// import {
+//   GoogleSignin,
+//   GoogleSigninButton,
+//   statusCodes,
+// } from "@react-native-community/google-signin";
 
-GoogleSignin.configure({
-  webClientId: "",
-  offlineAccess: true,
-});
+// GoogleSignin.configure({
+//   webClientId: "968385553762-6ocrsbdb8i0s1km4qs2cp1igdorad2q0.apps.googleusercontent.com",
+//   offlineAccess: true,
+// });
 
 export default class Login extends Component {
   constructor(props) {
@@ -19,31 +19,31 @@ export default class Login extends Component {
       loaded: false,
     };
   }
-  signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      this.setState({
-        userGoogleInfo: userInfo,
-        loaded: true,
-      });
-      console.log(this.state.userGoogleInfo);
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log("e 1");
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log("e 2");
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log("e 3");
-      } else {
-        console.log(error.message);
-      }
-    }
-  };
+  // signIn = async () => {
+  //   try {
+  //     await GoogleSignin.hasPlayServices();
+  //     const userInfo = await GoogleSignin.signIn();
+  //     this.setState({
+  //       userGoogleInfo: userInfo,
+  //       loaded: true,
+  //     });
+  //     console.log(this.state.userGoogleInfo);
+  //   } catch (error) {
+  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+  //       console.log("e 1");
+  //     } else if (error.code === statusCodes.IN_PROGRESS) {
+  //       console.log("e 2");
+  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+  //       console.log("e 3");
+  //     } else {
+  //       console.log(error.message);
+  //     }
+  //   }
+  // };
   render() {
     return (
       <View>
-        <GoogleSigninButton
+        {/* <GoogleSigninButton
           style={{ width: 222, height: 48 }}
           size={GoogleSigninButton.Size.Wide}
           color={GoogleSigninButton.Color.Dark}
@@ -60,44 +60,10 @@ export default class Login extends Component {
           </View>
         ) : (
           <Text>Not SignedIn</Text>
-        )}
+        )} */}
+        <Text>Hello World!!</Text>
       </View>
-
-      // <div className='container'>
-      //   <form onSubmit={handleSubmit} name={name}>
-      //     <div>
-      //       <label htmlFor='username'>
-      //         <small>Username</small>
-      //       </label>
-      //       <input name='username' type='text' />
-      //     </div>
-      //     <div>
-      //       <label htmlFor='password'>
-      //         <small>Password</small>
-      //       </label>
-      //       <input name='password' type='password' />
-      //     </div>
-      //     <div>
-      //       <button type='submit'>{displayName}</button>
-      //     </div>
-      //     {error && error.response && <div> {error.response.data} </div>}
-      //   </form>
-      // </div>
     );
   }
 }
 
-// const mapDispatch = (dispatch) => {
-// 	return {
-// 		handleSubmit(evt) {
-// 			evt.preventDefault();
-// 			const formName = evt.target.name;
-// 			const username = evt.target.username.value;
-// 			const password = evt.target.password.value;
-// 			dispatch(authenticate(username, password, formName));
-// 		},
-// 	};
-// };
-
-// export const Login = connect(mapLogin, mapDispatch)(AuthForm);
-// export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
