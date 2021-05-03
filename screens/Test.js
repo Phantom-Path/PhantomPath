520c5077be3817de3c7ee069d26f5cb57a8d021e
Altus Szawlowski <altusszawlowski@gmail.com>
2021-05-02 20:00:13 -0400

    rending text on chat

--- screens/Test.js
+++ screens/Test.js
@@ -1,64 +1,97 @@
-import React from "react";
+// @refresh reset
+
+import React, { useState, useEffect, useCallback } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
-import { Platform, KeyboardAvoidingView, SafeAreaView } from "react-native";
-import Fire from "../Fire";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
-export default class Test extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: [],
-    };
-  }
+const db = firebase.firestore();
+const chatsRef = db.collection("chats");
 
-  get user() {
-    return {
-      _id: Fire.uid, //adjust?
-      // name: this.props.navigation.state.params.name, //change
-    };
-  }
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
+  const [messages, setMessages] = useState([]);
 
-  componentDidMount() {
-    Fire.get((message) =>
-      this.setState((previous) => ({
-        message: GiftedChat.append(previous.messages, message),
-      }))
-    );
-  }
+  useEffect(() => {
+    readUser();
+    const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
+      const messagesFirestore = querySnapshot
+        .docChanges()
+        .filter(({ type }) => type === "added")
+        .map(({ doc }) => {
+          const message = doc.data();
+          //createdAt is firebase.firestore.Timestamp instance
+          //https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
+          return { ...message, createdAt: message.createdAt.toDate() };
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      appendMessages(messagesFirestore);
+    });
+    return () => unsubscribe();
+  }, []);
 
-  componentWillUnmount() {
-    Fire.off;
-  }
-
-  render() {
-    const chat = (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={Fire.send}
-        user={this.user}
-      />
-      // <GiftedChat
-      // messages={this.state.messages}
-      // onSend={Fire.send}
-      // user='altus'
-      // />
-    );
-
-    if (Platform.OS === "android") {
-      return (
-        <KeyboardAvoidingView
-          style={{ flex: 1 }}
-          behavior="padding"
-          keyboardVerticalOffset={30}
-          enabled
-        >
-          {chat}
-        </KeyboardAvoidingView>
+  const appendMessages = useCallback(
+    (messages) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages)
       );
+    },
+    [messages]
+  );
+
+  async function readUser() {
+    const user = await AsyncStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
     }
-    return <SafeAreaView style={{ flex: 1 }}>{chat}</SafeAreaView>;
   }
+  async function handlePress() {
+    const _id = Math.random().toString(36).substring(7);
+    const user = { _id, name };
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  }
+  async function handleSend(messages) {
+    const writes = messages.map((m) => chatsRef.add(m));
+    await Promise.all(writes);
+  }
+
+  // if (!user) {
+  //   return (
+  //     <View style={styles.container}>
+  //       <TextInput
+  //         style={styles.input}
+  //         placeholder="Enter your name"
+  //         value={name}
+  //         onChangeText={setName}
+  //       />
+  //       <Button onPress={handlePress} title="Enter the chat" />
+  //     </View>
+  //   );
+  // }
+  return <GiftedChat messages={messages} user={user} onSend={handleSend} />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    padding: 15,
+    marginBottom: 20,
+    borderColor: "gray",
+  },
+});
 
 //-------------------------------------------------------
 
